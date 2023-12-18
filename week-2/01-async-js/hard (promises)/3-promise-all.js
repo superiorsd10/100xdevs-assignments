0d179edfc2c9417881fd14b398f84c4d61a32102
superiorsd10 <sachindapkara6@gmail.com>
2023-12-18 17:23:59 +0530

    completing the hard section of 01-async-js in week2

--- week-2/01-async-js/hard (promises)/3-promise-all.js
+++ week-2/01-async-js/hard (promises)/3-promise-all.js
@@ -5,19 +5,35 @@
  */
 
 function wait1(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function wait2(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function wait3(t) {
-
+  return new Promise((resolve) => {
+    setTimeout(resolve, t * 1000);
+  });
 }
 
 function calculateTime(t1, t2, t3) {
+  const startTime = Date.now();
 
+  return Promise.all([wait1(t1), wait2(t2), wait3(t3)])
+    .then((result) => {
+      const endTime = Date.now();
+      const totalTime = endTime - startTime;
+      return totalTime;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 module.exports = calculateTime;
